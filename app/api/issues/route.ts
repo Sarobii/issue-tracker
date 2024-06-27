958feee51d998f0d8b03b692ab336dff84fecef4
Tim Crow <tcrow85@hotmail.com>
2024-06-27 13:10:20 -0400

    Build an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import {z} from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)})
+
+    
+export async function POST(request: NextRequest) {
+    if (!request) {
+        throw new Error('Request is null or undefined');
+    }
+    try {
+        const body = await request.json();
+        if (!body) {
+            throw new Error('Request body is null or undefined');
+        }
+        const validation = createIssueSchema.safeParse(body);
+        if (!validation.success) {
+            return NextResponse.json(validation.error.errors, { status: 400 });
+        }
+        const newIssue = await prisma.issue.create({
+            data: {title: body.title, description: body.description}
+        });
+        if (!newIssue) {
+            throw new Error('Failed to create new issue');
+        }
+        return NextResponse.json(newIssue, {status: 201});
+    } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}
